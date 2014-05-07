c4e0ab4b381ff38d2bcb69ed53198a6a29dcf9fa
Eric King <eric@webdeveric.com>
2014-05-07 15:07:12 -0500

    Used get_post_field instead of get_post

--- get-post-content-shortcode.php
+++ get-post-content-shortcode.php
@@ -8,16 +8,45 @@ Author URI: http://webdeveric.com/
 Plugin Group: Shortcodes
 */
 
+if ( ! function_exists('is_affirmative')):
+    function is_affirmative($arg)
+    {
+        if (is_string($arg))
+            $arg = strtolower($arg);
+        return in_array($arg, array( true, 'true', 'yes', 'y', '1', 1));
+    }
+endif;
+
 function wde_get_post_content_shortcode($atts, $shortcode_content = null, $code = '')
 {
     global $post;
-    $atts = shortcode_atts(array('id' => 0), $atts);
 
-    $atts['id'] = (int)$atts['id'];
+    $atts = shortcode_atts(
+        array(
+            'id'           => 0,
+            'wpautop'      => true,
+            'do_shortcode' => true
+        ),
+        $atts
+    );
+
+    $atts['id']           = (int)$atts['id'];
+    $atts['wpautop']      = is_affirmative($atts['wpautop']);
+    $atts['do_shortcode'] = is_affirmative($atts['do_shortcode']);
 
     if (isset($post, $post->ID) && $post->ID != $atts['id'] && $atts['id'] > 0) {
         $content = get_post_field('post_content', $atts['id']);
-        return is_wp_error($content) ? '' : do_shortcode(wpautop($content));
+
+        if (is_wp_error($content))
+            return '';
+
+        if ($atts['wpautop'])
+            $content = wpautop($content);
+
+        if ($atts['do_shortcode'])
+            $content = do_shortcode($content);
+
+        return $content;
     }
     return '';
 }
