a5dad76250ca927761a9590695aef636c44ba365
Eric King <eric@webdeveric.com>
2014-05-19 11:02:27 -0500

    Updated is_yes function to use strict comparison

--- get-post-content-shortcode.php
+++ get-post-content-shortcode.php
@@ -8,12 +8,12 @@ Author URI: http://webdeveric.com/
 Plugin Group: Shortcodes
 */
 
-if ( ! function_exists('is_affirmative')):
-    function is_affirmative($arg)
+if ( ! function_exists('is_yes')):
+    function is_yes($arg)
     {
         if (is_string($arg))
             $arg = strtolower($arg);
-        return in_array($arg, array( true, 'true', 'yes', 'y', '1', 1));
+        return in_array($arg, array(true, 'true', 'yes', 'y', '1', 1), true);
     }
 endif;
 
@@ -31,8 +31,8 @@ function wde_get_post_content_shortcode($atts, $shortcode_content = null, $code 
     );
 
     $atts['id']           = (int)$atts['id'];
-    $atts['wpautop']      = is_affirmative($atts['wpautop']);
-    $atts['do_shortcode'] = is_affirmative($atts['do_shortcode']);
+    $atts['wpautop']      = is_yes($atts['wpautop']);
+    $atts['do_shortcode'] = is_yes($atts['do_shortcode']);
 
     if (isset($post, $post->ID) && $post->ID != $atts['id'] && $atts['id'] > 0) {
         $content = get_post_field('post_content', $atts['id']);
