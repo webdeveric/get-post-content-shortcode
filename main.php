7f31bc72e6215ccd755f2ea512f4f45e294b7e30
Eric King <eric.king@lonelyplanet.com>
2017-01-04 16:02:24 -0600

    include_once to make tests pass

--- main.php
+++ main.php
@@ -2,11 +2,11 @@
 
 namespace webdeveric\GetPostContentShortcode;
 
-include 'src/functions.php';
-include 'src/Shortcode.php';
-include 'src/PostContentShortcode.php';
-include 'src/PostIDShortcode.php';
-include 'src/OuterPostIDShortcode.php';
+include_once 'src/functions.php';
+include_once 'src/Shortcode.php';
+include_once 'src/PostContentShortcode.php';
+include_once 'src/PostIDShortcode.php';
+include_once 'src/OuterPostIDShortcode.php';
 
 new PostContentShortcode();
 new PostIDShortcode();
